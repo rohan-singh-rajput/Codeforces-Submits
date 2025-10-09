9c6815279a02cb2d9b038c22d53c9fb8cbb4a7a9
rohan-singh-rajput <rohansinghrajput135@gmail.com>
2025-10-09 21:25:24 +0530

    October 9

---
+++ 800/A_Increase_or_Smash.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class A_Increase_or_Smash {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        while (n-- > 0) {
+            int size = sc.nextInt();
+            Set<Integer> st = new HashSet<>();
+
+            while (size-- > 0) {
+                int temp = sc.nextInt();
+                st.add(temp);
+            }
+
+            System.out.println(2 * st.size() - 1);
+
+        }
+    }
+}
